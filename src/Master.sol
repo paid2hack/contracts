2070e08ec7dbc2fbfa08dc506cf5f799ea9c4405
Ramesh Nair <ram@hiddentao.com>
2024-03-15 22:31:45 +0000

    feat: initial master/sponsor contracts

---
+++ src/Master.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: GPLv3
+pragma solidity ^0.8.24;
+
+import { Ownable } from "openzeppelin/access/Ownable.sol";
+
+struct Team {
+  string name;
+  address leader;
+  address[] members;
+}
+
+struct Event {
+  string name;
+  address owner;
+  uint[] teamIds;
+}
+
+error NotTeamLeader();
+error NotEventCreator();
+
+/**
+ * @dev Master contract holding all events and teams.
+ */
+contract Master is Ownable {
+  string public name;
+
+  uint public totalEvents;
+  mapping(uint => Event) public events;
+
+  uint public totalTeams;
+  mapping(uint => Team) public teams;
+
+  constructor() Ownable(msg.sender) {}
+
+  function createEvent(string memory _name) external {
+    totalEvents++;
+    events[totalEvents].owner = msg.sender;
+    events[totalEvents].name = _name;
+  }
+
+  function updateEventName(uint _eventId, string calldata _name) external isEventCreator(_eventId) {
+    events[_eventId].name = _name;
+  }
+
+  function createTeam(uint _eventId, Team calldata _team) external {
+    totalTeams++;
+    teams[totalTeams] = _team;
+    events[_eventId].teamIds.push(totalTeams);
+  }
+
+  function updateTeamMembers(uint _teamId, address[] calldata members) external isTeamLeader(_teamId) {
+    teams[_teamId].members = members;
+  }
+
+  function updateTeamName(uint _teamId, string calldata _name) external isTeamLeader(_teamId) {
+    teams[_teamId].name = _name;
+  }
+
+  // Modifiers
+
+  modifier isTeamLeader(uint _teamId) {
+    if (teams[_teamId].leader != msg.sender) {
+      revert NotTeamLeader();
+    }
+    _;
+  }
+
+  modifier isEventCreator(uint _eventId) {
+    if (events[_eventId].owner != msg.sender) {
+      revert NotEventCreator();
+    }
+    _;
+  }
+}
+
--- src/SponsorEscrow.sol
+++ src/SponsorEscrow.sol
@@ -6,9 +6,9 @@ import { Ownable } from "openzeppelin/access/Ownable.sol";
 contract SponsorEscrow is Ownable {
   string public name;
 
-  constructor(string memory name)
+  constructor(string memory _name)
     Ownable(_msgSender())
   {
-    this.name = name;
+    name = _name;
   }
 }

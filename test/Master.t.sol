70805d866ca2a60317a28056cab898119beadd27
Ramesh Nair <ram@hiddentao.com>
2024-03-16 13:39:18 +0000

    test: team fetching

--- test/Master.t.sol
+++ test/Master.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import {Test, console} from "forge-std/Test.sol";
 import { Vm } from "forge-std/Vm.sol";
 
-import { Master } from "../src/Master.sol";
+import { Master, Team, Event } from "../src/Master.sol";
 import { Ownable } from "openzeppelin/access/Ownable.sol";
 
 contract NaymTokenTest is Test {
@@ -24,5 +24,26 @@ contract NaymTokenTest is Test {
     assertEq(m.totalEvents(), 1);
     assertEq(m.getEvent(1).name, "Event 1");
     assertEq(m.getEvent(1).owner, owner1);
+  }
+
+  function test_createTeam() public {
+    vm.prank(owner1);
+    m.createEvent("Event 1");
+
+    vm.prank(owner1);
+    m.createTeam(1, Team({
+      name: "Team 1",
+      leader: owner1,
+      members: new address[](0)
+    }));
+
+    assertEq(m.totalTeams(), 1);
+    assertEq(m.getTeam(1).name, "Team 1");
+    assertEq(m.getTeam(1).leader, owner1);
+
+    (uint teamId_, Team memory team_) = m.getEventTeam(1, 0);
+    assertEq(teamId_, 1);
+    assertEq(team_.name, "Team 1");
+    assertEq(team_.leader, owner1);
   }
 }
